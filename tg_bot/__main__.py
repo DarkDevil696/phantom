9c81ff02527959d497433f80b5b5e476e3f851e0
MINDAPOOCHA <66699751+MINDAPOOCHA@users.noreply.github.com>
2020-06-10 16:43:35 +0400

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,25 +28,20 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 
 
-┈┈┈┈╱▔▔▔▔╲┈┈┈┈
-┈┈┈▕▕╲┊┊╱▏▏┈┈┈
-┈┈┈▕▕▂╱╲▂▏▏┈┈┈
-┈┈┈┈╲┊┊┊┊╱┈┈┈┈
-┈┈┈┈▕╲▂▂╱▏┈┈┈┈
-╱▔▔▔▔┊┊┊┊▔▔▔▔╲
-HOI    {}, MY NAME IS {} !
 
-I AM A GROUP MANAGER BOT MAINTAINED BY 
+HI    {}, MY NAME IS {} !
+
+I am a Group Manager Bot Maintained By 
 
 [THIS LEGEND](tg://user?id={}).
 
-HIT HELP FOR COMMANDS  /help
+HIT Help For Commands  /help
 
 I AM COMPLETELY OPEN SOURCE
 
 MY SOURCE CODE IS AVAILABLE TO YOU
 
-[HERE](https://github.com/leobrownlee/phantom)
+[HERE](https://github.com/MINDAPOOCHA/phantom)
 
 """
 
